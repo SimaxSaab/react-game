9a25858914ce55e1dfe7d6939f66d1150c93a230
SimaxSaab <minoreat@gmail.com>
2021-02-24 21:51:30 +0200

    Feat: server: console data.json

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+app.get('/select', function(req, res) {
+  let fileContent = fs.readFileSync(__dirname + "/data.json", "utf8");
+  console.log(fileContent);
+  res.end(fileContent);
+});
+
 app.listen(3030, function () {
   console.log('Server app listening on 3030!');
 });
