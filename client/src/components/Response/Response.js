4a53694f030976c97958e05fab0fffbd7aac613f
SimaxSaab <minoreat@gmail.com>
2021-02-24 21:12:40 +0200

    Init: init server

---
+++ client/src/components/Response/Response.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ResCard from './ResCard/ResCard';
+
+
+import t from './Response.module.css';
+
+export default class Scoreboard extends Component {
+
+  render() {
+    const {winner, playerPick, computerPick} = this.props;
+    // console.log(winner);
+    let winnerCaption;
+    if (winner == 1) { 
+      winnerCaption = 'You win!';
+    } else if(winner == -1) {
+      winnerCaption = 'You lose!';
+    } else { winnerCaption = 'It\'s draw'; }
+
+    return (
+      <main className="main">
+        <h1>{winnerCaption}</h1>
+        <div className="horizontal">
+          <ResCard winner={winner} pick={playerPick} cardAffiliation={false} />
+          <ResCard winner={winner} pick={computerPick} cardAffiliation={true} />
+        </div>
+        <button className={`${t.res__button}`} onClick={() => this.props.reset()}>Play again</button>
+      </main>
+    );
+  }
+}
